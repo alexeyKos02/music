e9e9f85c322e17820d543553ce877b14fa9054b2
alexeyKos02 <korolev.2002lexa@list.ru>
2023-05-26 13:49:55 +0300

    fix css navBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,8 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Col from 'react-bootstrap/Col';
 import Nav from 'react-bootstrap/Nav';
 import Row from 'react-bootstrap/Row';
 import Tab from 'react-bootstrap/Tab';
-import {Button} from "react-bootstrap";
 import "./NavBar.css"
 
 const NavBar = () => {
@@ -12,41 +11,48 @@ const NavBar = () => {
             <Row style={{marginLeft:"-5px"}}>
                 <Col sm={3} className="navBar" style={{marginLeft:"5px"}}>
                     <Nav variant="pills" className="flex-column">
-                        <Nav.Item>
-                            <Nav.Link eventKey="search">
-                                <div>
-                                    <svg className="inline-svg-icon" width="25px" height="25px">
-                                        <use xlinkHref="#search"></use>
-                                    </svg>
-                                </div>
-                                <span>Настройки</span>
-                            </Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link eventKey="home">
-                                    <svg width="25px" height="25px">
-                                        <use className="newImg" xlinkHref="#new"></use>
-                                    </svg>
-                            </Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link eventKey="save">
-                                <div>
-                                    <svg width="25px" height="25px">
-                                        <use xlinkHref="#save"></use>
-                                    </svg>
-                                </div>
-                            </Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link eventKey="settings">
-                                <div>
-                                    <svg className="inline-svg-icon" width="25px" height="25px">
-                                        <use xlinkHref="#settings"></use>
-                                    </svg>
-                                </div>
-                            </Nav.Link>
-                        </Nav.Item>
+                        <div className="d">
+                            <Nav.Item>
+                                <Nav.Link eventKey="search">
+                                    <div className="outline">
+                                        <svg className="inline-svg-icon" width="25px" height="25px">
+                                            <use xlinkHref="#search"></use>
+                                        </svg>
+                                    </div>
+                                    <span>Поиск</span>
+                                </Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link eventKey="home">
+                                    <div>
+                                        <svg width="25px" height="25px">
+                                            <use className="newImg" xlinkHref="#new"></use>
+                                        </svg>
+                                    </div>
+                                    <span>Главное</span>
+                                </Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link eventKey="save">
+                                    <div>
+                                        <svg width="25px" height="25px">
+                                            <use xlinkHref="#save"></use>
+                                        </svg>
+                                    </div>
+                                    <span>Коллекция</span>
+                                </Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link eventKey="settings">
+                                    <div>
+                                        <svg className="inline-svg-icon" width="25px" height="25px">
+                                            <use xlinkHref="#settings"></use>
+                                        </svg>
+                                    </div>
+                                    <span>Настройки</span>
+                                </Nav.Link>
+                            </Nav.Item>
+                        </div>
                     </Nav>
                 </Col>
                 <Col sm={9} style={{marginLeft: "60px"}}>
